1cc8883636b73ca6361b495aaa1cff2800afc879
chesangJ <mitingijoychesang@gmail.com>
2023-09-09 19:10:42 +0300

    added apis

--- warsan/api/views.py
+++ warsan/api/views.py
@@ -1,35 +1,50 @@
+from django.http import Http404
 from rest_framework.views import APIView
-from rest_framework.response  import Response
+from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.views import APIView
 from Immunization_Record.models import Immunization_Record
 from .serializer import Immunization_RecordSerializer
 
 class Immunization_recordView(APIView):
     def get(self, request):
-        immunization_record=Immunization_Record.objects.all()
-        serializers=Immunization_RecordSerializer (immunization_record, many=True)
-        return Response(serializers.data)
-    
+        immunization_records = Immunization_Record.objects.all()
+        serializer = Immunization_RecordSerializer(immunization_records, many=True)
+        return Response(serializer.data)
+
     def post(self, request):
         serializer = Immunization_RecordSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-   
+
+    def get(self, request, pk, format=None):
+        immunization = self.get_object(pk)
+        serializer = Immunization_RecordSerializer(immunization)
+        return Response(serializer.data)
+
+
+class ImmunizationRecordDetailView(APIView):
+    def get_object(self, pk):
+        try:
+            return Immunization_Record.objects.get(pk=pk)
+        except Immunization_Record.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        immunization = self.get_object(pk)
+        serializer = Immunization_RecordSerializer(immunization)
+        return Response(serializer.data)
 
     def put(self, request, pk, format=None):
-        immunization = Immunization_Record.objects.get(pk)
+        immunization = self.get_object(pk)
         serializer = Immunization_RecordSerializer(immunization, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-     
     def delete(self, request, pk, format=None):
-        immunization= self.get_object(pk)
+        immunization = self.get_object(pk)
         immunization.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
