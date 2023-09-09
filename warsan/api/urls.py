360ea4f01fc649b81c43c10fa39e2f1e8ca939f8
kayitaclever <kayitaramirwaaclever>
2023-09-09 19:36:30 +0300

    created ChildGuardian model

--- warsan/api/urls.py
+++ warsan/api/urls.py
@@ -2,16 +2,9 @@ from django.urls import path
 from .views import GuardianListView, GuardianDetailView
 from .views import ChildListView, ChildDetailView
 
-
-ChildProcessError
-
 urlpatterns = [
-
-    path('guardian/',GuardianListView.as_view(),name='guardian_list_view'),
-    path('guardian/',GuardianDetailView.as_view(),name='guardian_detail_view')
-    path('guardian/',ChildDetailView.as_view(),name='guardian_list_view'),
-    path('guardian/',ChildDetailView.as_view(),name='guardian_detail_view'),
-    
-
-    
+    path('guardian/', GuardianListView.as_view(), name='guardian_list_view'),
+    path('guardian/<int:pk>/', GuardianDetailView.as_view(), name='guardian_detail_view'),
+    path('child/', ChildListView.as_view(), name='child_list_view'),
+    path('child/<int:pk>/', ChildDetailView.as_view(), name='child_detail_view'),
 ]
