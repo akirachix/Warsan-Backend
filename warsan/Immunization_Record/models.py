fca25e329923e5341e64924f942930868ed96e45
chesangJ <mitingijoychesang@gmail.com>
2023-09-09 14:39:02 +0300

    added relations between vaccine and child

--- warsan/Immunization_Record/models.py
+++ warsan/Immunization_Record/models.py
@@ -1,6 +1,9 @@
 from django.db import models
-
+# from vaccine.models import Vaccine
+# from child.models import Child
 class Immunization_Record(models.Model): 
+    # child=models.OneToOneField(Child,on_delete=models.CASCADE,null=true)
+    # vaccine=models.ForeignKey(Vaccine,on_delete=models,CASCADE,null=true)
     date_of_administaration=models.DateTimeField()
     next_date_of_administration= models.DateTimeField()
    
