f0b5f10c60e247dcdb39567d788c518a28622e7f
chesangJ <mitingijoychesang@gmail.com>
2023-09-09 13:19:40 +0300

    deleted form.py

--- warsan/Immunization_Record/admin.py
+++ warsan/Immunization_Record/admin.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from .models import Immunization_Record
 class ImmunizationAdmin(admin.ModelAdmin):
     list_display= ("date_of_administaration","next_date_of_administration")
-
 admin.site.register(Immunization_Record,ImmunizationAdmin)   
 
 
--- warsan/Immunization_Record/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Immunization_Record
-
-class ImmunizationUploadForm(forms.ModelForm):
-    class Meta:
-        model=Immunization_Record
-        fields="__all__"
