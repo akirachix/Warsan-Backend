92f4d305cfc90f54d99506f87166123d6df395b3
chesangJ <mitingijoychesang@gmail.com>
2023-09-10 21:08:56 +0300

    added api for DELETE and PUT

--- warsan/api/urls.py
+++ warsan/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import Immunization_recordView
+from .views import Immunization_recordView,ImmunizationDetailView
 
 urlpatterns = [
+
     path('immunization_record/', Immunization_recordView.as_view(), name='immunization_record_list_view'),
-    path('immunization_record/<int:id>/', Immunization_recordView.as_view(), name='immunization_record_detail_view'),
+    path('immunization_record/<int:pk>/', ImmunizationDetailView.as_view(), name='immunization_record_detail_view'),  # Use 'pk' instead of 'id'
+
 ]
--- warsan/api/views.py
+++ warsan/api/views.py
@@ -18,32 +18,27 @@ class Immunization_recordView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-
-
 class ImmunizationDetailView(APIView):
-    def get_object(self, ):
+    def get_object(self, pk):
         try:
-            return Immunization_Record.objects.get(id=id)
+            return Immunization_Record.objects.get(pk=pk)
         except Immunization_Record.DoesNotExist:
             raise Http404
 
-    def get(self, request, id, format=None):
-        immunization = self.get_object(id)
+    def get(self, request, pk):
+        immunization = self.get_object(pk)
         serializer = Immunization_RecordSerializer(immunization)
         return Response(serializer.data)
 
-    def put(self, request, id, format=None):
-        immunization = self.get_object(id)
+    def put(self, request, pk):
+        immunization = self.get_object(pk)
         serializer = Immunization_RecordSerializer(immunization, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
 
-    def delete(self, request, id, format=None):
-        immunization = self.get_object(id)
+    def delete(self, request, pk):
+        immunization = self.get_object(pk)
         immunization.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)   
-
+        return Response(status=status.HTTP_204_NO_CONTENT)
