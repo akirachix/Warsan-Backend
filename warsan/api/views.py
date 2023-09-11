c79f31c5892af5371003a37681e49fb3f21a124b
Eunice Musenyia <124151285+EuniceMusenyia@users.noreply.github.com>
2023-09-11 17:14:38 +0300

    added the try block to my put,get and delete functions

--- warsan/api/views.py
+++ warsan/api/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -27,19 +28,36 @@ class VaccineDetailView(APIView):
             raise Http404
 
     def get(self, request, pk):
-        vaccine = self.get_object(pk)
-        serializer = VaccineSerializer(vaccine)
-        return Response(serializer.data)
+        try:
+            vaccine = self.get_object(pk)
+            serializer = VaccineSerializer(vaccine)
+            return Response(serializer.data)
+        except Http404:
+             return Response("Vaccine not found", status=status.HTTP_404_NOT_FOUND)
+
 
     def put(self, request, pk):
-        vaccine = self.get_object(pk)
-        serializer = VaccineSerializer(vaccine, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            vaccine = self.get_object(pk)
+            serializer = VaccineSerializer(vaccine, data=request.data)
+            if serializer.is_valid():
+               serializer.save()
+               return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Http404:
+           return Response("Vaccine not found", status=status.HTTP_404_NOT_FOUND)
+
 
     def delete(self, request, pk):
-        vaccine = self.get_object(pk)
-        vaccine.delete()
-        return Response("Vaccine Deleted",status=status.HTTP_204_NO_CONTENT)
+        try:
+            vaccine = self.get_object(pk)
+            vaccine.delete()
+            return Response("Vaccine Deleted", status=status.HTTP_204_NO_CONTENT)
+        except Http404:
+            return Response("Vaccine not found", status=status.HTTP_404_NOT_FOUND)
+
+
+
+
+
+
