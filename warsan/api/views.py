610dfdd77aea352ca01be8f92ba4a4e6e8dc0575
chesangJ <mitingijoychesang@gmail.com>
2023-09-12 14:03:58 +0300

    added the get put and delete in a try block

--- warsan/api/views.py
+++ warsan/api/views.py
@@ -18,37 +18,40 @@ class Immunization_recordView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+    
 class ImmunizationDetailView(APIView):
-    def get_object(self, pk):
+     def immunization_detail(self, request, pk, operation):
         try:
-            return Immunization_Record.objects.get(pk=pk)
+            immunization_Record = Immunization_Record.objects.get(pk=pk)
+            if operation == 'get':
+                serializer = Immunization_RecordSerializer(immunization_Record)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            elif operation == 'put':
+                immunization_record = Immunization_Record.objects.get(pk=pk)
+                serializer = Immunization_RecordSerializer(immunization_record, data=request.data)
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response(serializer.data, status=status.HTTP_200_OK)
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            elif operation == 'delete':
+                Immunization_Record.delete()
+                return Response("Record deleted", status=status.HTTP_204_NO_CONTENT)
         except Immunization_Record.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        try:
-            immunization = self.get_object(pk)
-            serializer = Immunization_RecordSerializer(immunization)
-            return Response(serializer.data)
-        except Immunization_Record.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            return Response("Record not found", status=status.HTTP_404_NOT_FOUND)
         
-    def put(self, request, pk):
-        try:
-            immunization = self.get_object(pk)
-            serializer = Immunization_RecordSerializer(immunization, data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        except Immunization_Record.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-   
-    def delete(self, request, pk):
-        try:
-            immunization = self.get_object(pk)
-            immunization.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        except Immunization_Record.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+     def get(self, request, pk, format=None):
+        return self.immunization_detail(request, pk, 'get')
+     
+     def put(self, request, pk, format=None):
+        return self.immunization_detail(request, pk, 'put')
+     
+     def delete(self, request, pk, format=None):
+        return self.immunization_detail(request, pk, 'delete')
+
+
+
+
+
+
+
+
