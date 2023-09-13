1b6e9ea3c0ffa3a1f0bfe2b3d17b2184ac7287fb
swanjikumainaa <swanjikumainaa@gmail.com>
2023-09-13 19:57:12 +0300

    added tests

--- warsan/api/tests.py
+++ warsan/api/tests.py
@@ -2,35 +2,28 @@ from django.test import TestCase
 from rest_framework.test import APIClient
 from rest_framework import status
 from location.models import Location
-from .serializers import LocationSerializer  
-# Create your tests here.
+from .serializers import LocationSerializer
 
 class LocationViewTests(TestCase):
     def setUp(self):
-        self.client = APIClient()  
+        self.client = APIClient()
         self.location_data = {
             'state': 'Somaliland',
             'region': 'Awdal',
             'district': 'Baki',
-           
         }
         self.location = Location.objects.create(**self.location_data)
 
+    def test_list_locations(self):
+        response = self.client.get('/api/location/') 
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
 
-def test_list_locations(self):
-    response = self.client.get('http://127.0.0.1:8000/api/location/')
-    self.assertEqual(response.status_code, status.HTTP_200_OK)
-    self.assertEqual(len(response.data), 1)  
-
-def test_create_location(self):
-    response = self.client.post('http://127.0.0.1:8000/api/location/', self.location_data)
-    self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-    self.assertEqual(Location.objects.count(), 2)  
-    self.assertEqual(Location.objects.last().state, 'Test State')
-
-def test_get_location_detail(self):
-    response = self.client.get(f'http://127.0.0.1:8000/api/location/4/')
-    self.assertEqual(response.status_code, status.HTTP_200_OK)
-    self.assertEqual(response.data['state'], 'Test State')
-
-
+    def test_create_location(self):
+        response = self.client.post('/api/location/', self.location_data)  
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Location.objects.count(), 2)
+        self.assertEqual(Location.objects.last().state, 'Somaliland') 
+        
+    def test_get_location_detail(self):
+        response = self.client.get(f'/api/location/{self.location.id}/')  
