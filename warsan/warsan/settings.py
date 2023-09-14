23739c9fe784d6e238cdbd9c9639ce05f4f169fe
Eunice Musenyia <124151285+EuniceMusenyia@users.noreply.github.com>
2023-09-14 18:02:11 +0300

    removed database credentials

--- warsan/warsan/settings.py
+++ warsan/warsan/settings.py
@@ -78,18 +78,28 @@ WSGI_APPLICATION = 'warsan.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'warsan',
+        
+#         'USER': 'warsanuser',         
+#         'PASSWORD': 'warsanpass',   
+#         'HOST': 'localhost',          
+#         'PORT': '5432',                  
+#     }
+# }
+import os
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'warsan',
-        
-        'USER': 'warsanuser',         
-        'PASSWORD': 'warsanpass',   
-        'HOST': 'localhost',          
-        'PORT': '5432',                  
+        'ENGINE': os.environ.get('DB_ENGINE'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
-
 
 # 
 REST_FRAMEWORK = {
