c69a201baaf33ff9ac91f89484507cc7323e28ab
kilas-Aniso <124134048+kilas-Aniso@users.noreply.github.com>
2023-09-18 03:04:10 +0300

    updates

--- child/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render, redirect
-from .models import Child, Guardian
-
-# You can add more views here as needed
-
--- vaccine/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Vaccine
-
-class VaccineAdmin(admin.ModelAdmin):
-    list_display = ("name", "target_disease", "minimum_age", "maximum_age", "recommended_dose", "date_created")
-admin.site.register(Vaccine,VaccineAdmin)
